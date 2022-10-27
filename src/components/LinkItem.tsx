dc71d47f1b48371268cd1f668acde0c18149b46d
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-10-28 00:21:50 +0100

    Adding subtexts to links

--- src/components/LinkItem.tsx
+++ src/components/LinkItem.tsx
@@ -2,15 +2,17 @@ import React from "react";
 import classes from "./LinkItem.module.css";
 
 interface Props {
-	children: React.ReactNode;
+	title: string;
 	url: string;
 	id?: string;
+	children?: React.ReactNode;
 }
 
-export default function LinkItem({ children, url, id }: Props) {
+export default function LinkItem({ children, url, id, title }: Props) {
 	return (
 		<a className={classes.item} id={id} href={url} target="_blank">
-			{children}
+			{title}
+			<p>{children}</p>
 		</a>
 	);
 }
--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -7,21 +7,21 @@ import SlackSVG from "../assets/slack.svg";
 export default function Links() {
 	return (
 		<main className={classes.container}>
-			<LinkItem url="https://twitter.com/The_Jazzmann">Twitter Link</LinkItem>
-			<LinkItem url="https://training.zuri.team" id="btn__zuri">
-				Zuri Team
+			<LinkItem title="Twitter Link" url="https://twitter.com/The_Jazzmann" />
+			<LinkItem title="Zuri Team" url="https://training.zuri.team" id="btn__zuri" />
+			<LinkItem title="Zuri Books" url=" http://books.zuri.team" id="books">
+				I visit this site anytime I need to find a great book about coding
 			</LinkItem>
-			<LinkItem url=" http://books.zuri.team" id="books">
-				Zuri Books
+			<LinkItem title="Python Books" url="https://books.zuri.team/python-for-beginners?ref_id=abotsjzm" id="book__python">
+				I found this book particularly useful, especially as a beginner. It sets you up and running on your journey to become the greatest programmer
+				that the world has ever seen
 			</LinkItem>
-			<LinkItem url="https://books.zuri.team/python-for-beginners?ref_id=abotsjzm" id="book__python">
-				Python Books
+			<LinkItem title="Background Check for Coders" url="https://backgroundcheck.zuri.team" id="pitch">
+				Are you an entrepreneur looking for trustworthy coders to hire? Run background checks on your employees to learn about their good, bad and
+				downright scary activities
 			</LinkItem>
-			<LinkItem url="https://backgroundcheck.zuri.team" id="pitch">
-				Background Check for Coders
-			</LinkItem>
-			<LinkItem url="https://books.zuri.team/design-rules" id="book__design">
-				Design Books
+			<LinkItem title="Design Books" url="https://books.zuri.team/design-rules" id="book__design">
+				This Design Rules Book by HNG gives you all the tips & guidelines you need to create truly professional designs!
 			</LinkItem>
 			<div className={classes.bottom}>
 				<div>
