360e5d6ea6611d0efa36f3c6eaf9bf55e146a25c
abotstheboss <abolarinoluwatobistephen@gmail.com>
2022-10-27 22:02:34 +0100

    Links section updated

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import LinkItem from "./LinkItem";
 import classes from "./Links.module.css";
+import GithubSVG from "../assets/Github.svg";
+import SlackSVG from "../assets/slack.svg";
 
 export default function Links() {
 	return (
@@ -11,6 +13,14 @@ export default function Links() {
 			<LinkItem>Python Books</LinkItem>
 			<LinkItem>Background Check for Coders</LinkItem>
 			<LinkItem>Design Books</LinkItem>
+			<div className={classes.bottom}>
+				<div>
+					<img src={SlackSVG} alt="slack" />
+				</div>
+				<div>
+					<img src={GithubSVG} alt="github" />
+				</div>
+			</div>
 		</div>
 	);
 }
